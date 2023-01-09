a16b5770d011495a7f093036c01b61a0d7ae05bb
vlameli <75256335+vlameli@users.noreply.github.com>
2023-01-09 14:43:02 +0100

    Update octokit.js
    Signed-off-by: vlameli <75256335+vlameli@users.noreply.github.com>

--- .github/actions/octokit.js
+++ .github/actions/octokit.js
@@ -1,14 +1,6 @@
-// GITHUB_EVENT_PATH always exists when run by an Action,
-// see https://git.io/JvUf7 for a full list
-const eventPayload = require(process.env.GITHUB_EVENT_PATH);
+// .github/actions/octokit.js
 const { Octokit } = require("@octokit/action");
 
-createPrComment();
+const octokit = new Octokit();
 
-async function createPrComment() {
-  // No need to pass process.env.GITHUB_TOKEN, `@octokit/action`
-  // is using it directly and throws an error if it is not present.
-  const octokit = new Octokit();
-
-  console.log("Comment created: %d", data.html_url);
-}
+// `octokit` is now authenticated using GITHUB_TOKEN
